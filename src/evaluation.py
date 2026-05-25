68d6d2a6f4a55ffc1b33c0b4d71b564605f70504
Bomin Chuang <appppm31012@gmail.com>
2026-05-25 22:34:43 +0900

    fix: some variables to constant and fix the evaluation method.

--- src/evaluation.py
+++ src/evaluation.py
@@ -7,17 +7,17 @@ from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
 from tqdm import tqdm
 
-from src.config import embedding_model, reranker_model, url
+from src.config import DATASET_URL, EMBEDDING_MODEL, RERANKER_MODEL, retriever_type, search_k
 from src.retriever import HybridRetriever, RAGRetriever, initialize_vector_database
 
 
-def evaluate_retriever(retriever_type: str = "hybrid"):
+def evaluate_retriever(retriever_type: str = retriever_type, split: str = "dev") -> None:
     """
     Evaluate the retriever performance using Recall@30 for initial retrieval and NDCG@3 for reranked results.
     """
     log_dir = os.path.join(os.path.dirname(__file__), "..", "log")
     os.makedirs(log_dir, exist_ok=True)
-    log_file_path = os.path.join(log_dir, f"evaluation_nfcorpus_{retriever_type}.log")
+    log_file_path = os.path.join(log_dir, f"nfcorpus_{retriever_type}_{split}.log")
 
     logging.basicConfig(
         level=logging.INFO,
@@ -26,27 +26,26 @@ def evaluate_retriever(retriever_type: str = "hybrid"):
     )
 
 
-    data_path = util.download_and_unzip(url, os.path.join(os.path.dirname(__file__), "..", "datasets"))
-    corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
+    data_path = util.download_and_unzip(DATASET_URL, os.path.join(os.path.dirname(__file__), "..", "datasets"))
+    corpus, queries, qrels = GenericDataLoader(data_path).load(split=split)
     
     # print(queries.items()) # {'PLAIN-2': 'Do Cholesterol Statin Drugs Cause Breast Cancer?'}
-
+    # Ensure the vector database is initialized before evaluation
     db_directory = os.path.join(os.path.dirname(__file__), "..", "vectordatabase")
-    initialize_vector_database(db_directory)
+    initialize_vector_database(db_directory)  
 
-    search_k = 100
     if retriever_type == "hybrid":
         ragretriever = HybridRetriever(
             db_directory=db_directory,
-            embedding_model=embedding_model,
-            reranker_model=reranker_model,
+            embedding_model=EMBEDDING_MODEL,
+            reranker_model=RERANKER_MODEL,
             search_k=search_k
         )
     elif retriever_type == "vector":
         ragretriever = RAGRetriever(
             db_directory=db_directory,
-            embedding_model=embedding_model,
-            reranker_model=reranker_model,
+            embedding_model=EMBEDDING_MODEL,
+            reranker_model=RERANKER_MODEL,
             search_k=search_k
         )
     else:
@@ -54,20 +53,18 @@ def evaluate_retriever(retriever_type: str = "hybrid"):
         
     logging.info(f"--- Evaluating Initial Retrieval (Top {search_k}) ---")
     initial_results = {}
+    retriever_cache_result = {}
     for query_id, query_text in tqdm(queries.items(), desc="Initial Retriever"):
-        initial_docs = ragretriever.retriever.invoke(query_text) # will return the list that already sorted by the retriever
+        retrieved_chunks = ragretriever.retriever.invoke(query_text) # will return the list that already sorted by the retriever
+        retriever_cache_result[query_id] = retrieved_chunks
         query_results ={}
-        for rank, doc in enumerate(initial_docs):
-            # print(f"\n=== Rank: {rank} ===")
-            # print(f"Doc ID: {doc.metadata.get('id')}")
-            # print(f"Doc Content preview: {doc.page_content[:100]}...")
-            # print(f"Doc Metadata: {doc.metadata}")
-            
-            if doc.metadata["id"] not in query_results:
-                query_results[doc.metadata["id"]] = 1.0 / (rank + 1) # for BEIR to calculate the metrics, we need to assign a score to each retrieved document.
-
+        for rank, chunk in enumerate(retrieved_chunks):            
+            if chunk.metadata["id"] not in query_results:
+                query_results[chunk.metadata["id"]] = 1.0 / (rank + 1) # for BEIR to calculate the metrics, we need to assign a score to each retrieved document.
+        query_results = dict(sorted(query_results.items(), key=lambda x: x[1], reverse=True)[:search_k])
         initial_results[query_id] = query_results
 
+    
     # Evaluate initial retrieval using Recall@30
     evaluator = EvaluateRetrieval()
     k_values_initial = [1,5,10,30]
@@ -78,26 +75,26 @@ def evaluate_retriever(retriever_type: str = "hybrid"):
     # {'Recall@1': 0.05611, 'Recall@5': 0.12886, 'Recall@10': 0.12886, 'Recall@30': 0.12886}, 
     # {'P@1': 0.43963, 'P@5': 0.31641, 'P@10': 0.1582, 'P@30': 0.05273})  
     logging.info("Initial Retrieval Metrics:")
-    logging.info(f"Recall@30: {recall['Recall@30']}") # Vector: 0.12886,  Hybrid: 0.12886
+    logging.info(f"NDCG@10: {ndcg['NDCG@10']}")
+    logging.info(f"Recall@30: {recall['Recall@30']}")
 
 
     reranked_results = {}
     for query_id, query_text in tqdm(queries.items(), desc="Reranking"):
-        initial_docs = ragretriever.retriever.invoke(query_text) # will use the init paramaters of the retriever, which is search_k
-         # print(initial_docs)
-        if not initial_docs:
+        retrieved_chunks = retriever_cache_result[query_id] 
+        if not retrieved_chunks:
             reranked_results[query_id] = {}
             continue
-        pairs = [(query_text, doc.page_content) for doc in initial_docs]
+        pairs = [(query_text, chunk.page_content) for chunk in retrieved_chunks]
         scores = ragretriever.reranker.predict(pairs)
-        scored_docs = sorted(zip(scores, initial_docs), key=lambda x: x[0], reverse=True)
+        scored_chunks = sorted(zip(scores, retrieved_chunks), key=lambda x: x[0], reverse=True)
 
         query_results = {}
         seen_ids = set()
-        for score, doc in scored_docs:
-            if doc.metadata["id"] not in seen_ids:
-                query_results[doc.metadata["id"]] = float(score)
-                seen_ids.add(doc.metadata["id"])
+        for score, chunk in scored_chunks:
+            if chunk.metadata["id"] not in seen_ids:
+                query_results[chunk.metadata["id"]] = float(score)
+                seen_ids.add(chunk.metadata["id"])
         reranked_results[query_id] = query_results
 
     # Evaluate reranked results using NDCG@3
@@ -111,5 +108,6 @@ def evaluate_retriever(retriever_type: str = "hybrid"):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate Retriever Models")
     parser.add_argument("--retriever", type=str, choices=["vector", "hybrid"], default="hybrid", help="Choose the retriever to evaluate")
+    parser.add_argument("--split", type=str, choices=["train", "dev", "test"], default="dev", help="Choose the dataset split to evaluate on")
     args = parser.parse_args()
-    evaluate_retriever(retriever_type=args.retriever)
+    evaluate_retriever(retriever_type=args.retriever, split=args.split)
