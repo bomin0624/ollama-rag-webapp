74058c4bda71990a3f44ed4c06bdddde354581b4
Bomin Chuang <appppm31012@gmail.com>
2026-05-25 11:57:45 +0900

    fix some config variables

--- src/config.py
+++ src/config.py
@@ -1,13 +1,15 @@
 
 dataset = "nfcorpus"
-url = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset}.zip"
+DATASET_URL = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset}.zip"
 
+search_k = 200
 
+EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
 
-embedding_model = "BAAI/bge-large-en-v1.5"
-
-reranker_model = "BAAI/bge-reranker-v2-m3"
+RERANKER_MODEL = "BAAI/bge-reranker-v2-m3"
 
 OLLAMA_URL = "http://localhost:11434"  # Ollama server URL
 
-GENERATE_MODEL = "llama3.1"  # Model name for generation
+generate_model = "llama3.1"  # Model name for generation
+
+retriever_type = "hybrid"  # Options: "hybrid" or "vector"
